b61db8d560013317d6243c2842f64c2bb2618c93
Oleksandr <argamakov2213@gmail.com>
2023-08-20 23:11:38 +0300

    add event on button

---
+++ app.js
@@ -0,0 +1,17 @@
+
+// const of inputs
+const wattage = document.querySelector("input.wattage-input");
+const hour = document.querySelector("input.hour-input");
+const voltage = document.querySelector("input.voltage-input");
+const capacityFactor = document.querySelector("input.capacityFactor-input");
+
+// function to find capacity
+const btnCapacity = document.querySelector("button.btn-capacity");
+btnCapacity.addEventListener("click", ()=>{
+    let w = wattage.value;
+    let h = hour.value;
+    let v = voltage.value;
+    let k = capacityFactor.value;
+    const capacityOfAccumulator = Number((w * h) / (v * k));
+     console.log("Ah:",Math.ceil(capacityOfAccumulator)); 
+});
