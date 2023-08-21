b0c289b3163ed83cdb50e8d1482c77212be3ca73
Oleksandr <argamakov2213@gmail.com>
2023-08-21 12:03:13 +0300

    fix with prettier

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 // const of inputs
 const wattage = document.querySelector("input.wattage-input");
 const hour = document.querySelector("input.hour-input");
@@ -7,11 +6,11 @@ const capacityFactor = document.querySelector("input.capacityFactor-input");
 
 // function to find capacity
 const btnCapacity = document.querySelector("button.btn-capacity");
-btnCapacity.addEventListener("click", ()=>{
-    let w = wattage.value;
-    let h = hour.value;
-    let v = voltage.value;
-    let k = capacityFactor.value;
-    const capacityOfAccumulator = Number((w * h) / (v * k));
-     console.log("Ah:",Math.ceil(capacityOfAccumulator)); 
+btnCapacity.addEventListener("click", () => {
+  let w = wattage.value;
+  let h = hour.value;
+  let v = voltage.value;
+  let k = capacityFactor.value;
+  const capacityOfAccumulator = Number((w * h) / (v * k));
+  console.log("Ah:", Math.ceil(capacityOfAccumulator));
 });
